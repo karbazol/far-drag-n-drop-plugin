c63601bb6107b418b0b1162d328ed359c7680edc
Eugene Leskinen <karbazol@gmail.com>
2018-06-21 16:35:07 +0300

    Bumped version to 3.0.85.4700
    Bumped version to 3.0.85.4700.
    Made the last number in the version to be defined by FAR_MINIMAL_BUILD_SUPPORTED
    environmental variable.

--- dragndrop/msvs2015/generated/version.h
+++ dragndrop/msvs2015/generated/version.h
@@ -1,9 +1,9 @@
-#define PRODUCT_VERSION 3,0,84,4700
-#define PRODUCT_VERSION_STRING "3.0.84.4700"
+#define PRODUCT_VERSION 3,0,85,4700
+#define PRODUCT_VERSION_STRING "3.0.85.4700"
 #define FILE_VERSION PRODUCT_VERSION
 #define FILE_VERSION_STRING PRODUCT_VERSION_STRING
 
 #define COMPANY_NAME "Eugene Leskinen"
 #define PRODUCT_NAME "Drag & drop plug-in for FAR manager"
-#define LEGAL_COPYRIGHT "2005-2017 Eugene Leskinen et al."
+#define LEGAL_COPYRIGHT "2005-2018 Eugene Leskinen et al."
 /* file automagically generated */
